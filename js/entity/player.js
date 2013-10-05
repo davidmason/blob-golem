da1d1155a011d364783cee5cd659eb2efe469b27
David Mason <dr.d.mason@gmail.com>
2013-10-05 10:43:55 +1000

    add pause and basic player movement keys

--- js/entity/player.js
+++ js/entity/player.js
@@ -7,3 +7,21 @@ inherits(Player, CustomEntity);
 function Player(options) {
   this.loadOptions(options);
 }
+
+Player.prototype.keyboardInput = function(keyboard){
+  if ('A' in keyboard.keysDown){
+    this.velocity.x = -this.speed;
+  }
+
+  if ('D' in keyboard.keysDown){
+    this.velocity.x = this.speed;
+  }
+
+  if ('W' in keyboard.keysDown){
+    this.velocity.y = -this.speed;
+  }
+
+  if ('S' in keyboard.keysDown){
+    this.velocity.y = this.speed;
+  }
+};
